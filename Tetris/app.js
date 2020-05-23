b48ebea9829a757b8f2307beea6d0fcdc493c4f3
aquariumm <aquariumm@users.noreply.github.com>
2020-05-22 21:31:33 -0400

    Add files via upload

--- Tetris/app.js
+++ Tetris/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const StartBtn = document.getElementById('#start-button')
     const ScoreDisplay = document.getElementById('#score')
+    let timerId
 
     const lShapeTetromino = [
         [1, width + 1, width * 2 + 1, 2],
@@ -11,4 +12,58 @@ document.addEventListener('DOMContentLoaded', () => {
         [1, width + 1, width * 2 + 1, width * 2],
         [width, width * 2, width * 2 + 1, width * 2 + 2]
     ]
+
+    const oShapeTetromino = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ]
+    const tShapeTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ]
+    const iShapeTetromino = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ]
+    const zShapeTetromino = [
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
+    ]
+    const tetrominoes = [lShapeTetromino, iShapeTetromino, oShapeTetromino, tShapeTetromino, zShapeTetromino]
+
+    let currentPosition = 4
+    let currentRotation = 0
+
+    let random = Math.floor(Math.random() * tetrominoes.length)
+
+    let current = tetrominoes[random][currentRotation]
+
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    timerId = setInterval(moveDown, 1000)
+
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+    }
 })
